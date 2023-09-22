1363e76498d09c61568aa8121c49ea1e7ef4c1d1
Metehan KARAKAYA <karakayametehan00@gmail.com>
2023-09-22 18:46:59 +0300

    Add new strings

--- lib/presentation/resources/string_manager.dart
+++ lib/presentation/resources/string_manager.dart
@@ -3,6 +3,8 @@ class AppString {
   static const String noRouteFound = "No Route Found!";
 
   static const String appName = "Colorify";
+  static const String menu = "Menu";
+  static const String lastColors = "Last 20 Colors";
   static const String red = "Red";
   static const String green = "Green";
   static const String blue = "Blue";
